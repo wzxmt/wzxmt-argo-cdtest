84d158794b32737b760127a2eeb0ac3b68cf218a
k8s <k8s@163.com>
2021-08-29 00:36:07 +0800

    first commit

---
+++ delta-amss/README.md
@@ -0,0 +1,33 @@
+# deployer-deltademo
+
+1、创建名称空间
+
+```bash
+kubectl create ns delta
+```
+
+2、创建docker-registry
+
+```
+cat << EOF >registry-secret.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  namespace: delta
+  name: harborlogin
+type: kubernetes.io/dockerconfigjson
+data:
+  .dockerconfigjson: $(cat $HOME/.docker/config.json |base64 -w 0)
+EOF
+kubectl create -f registry-secret.yaml
+```
+
+3、部署服务
+
+```
+kubectl apply -f ./
+```
+
+# argo-cdtest
+# argo-cdtest
+# argo-cdtest
---
+++ delta-amss/pull.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker pull bridge5/kaide-frontend:nightly
+docker pull bridge5/service-accounts:latest
+docker pull bridge5/service-administrates:latest
+docker pull bridge5/service-auditlogs:latest
+docker pull bridge5/service-capitaldatas:latest
+docker pull bridge5/service-gateways:latest
+docker pull bridge5/service-imvoffice:latest
+docker pull bridge5/service-imvoffice:latest
+docker pull bridge5/service-imvretail:latest
+docker pull bridge5/service-mixbudget:latest
